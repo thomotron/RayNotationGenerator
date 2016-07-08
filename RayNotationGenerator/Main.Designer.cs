34b2b95c273c556e9a09def69717bd90a534f425
Tom <gigatech55@gmail.com>
2016-07-08 17:58:13 +0930

    Add project files.

---
+++ RayNotationGenerator/Main.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace RayNotationGenerator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SizeSelect = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.SizeSelect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textbox
+            // 
+            this.textbox.Location = new System.Drawing.Point(12, 12);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(268, 20);
+            this.textbox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save As...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Encode);
+            // 
+            // SizeSelect
+            // 
+            this.SizeSelect.Cursor = System.Windows.Forms.Cursors.Default;
+            this.SizeSelect.Location = new System.Drawing.Point(75, 39);
+            this.SizeSelect.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.SizeSelect.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.SizeSelect.Name = "SizeSelect";
+            this.SizeSelect.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.SizeSelect.Size = new System.Drawing.Size(57, 20);
+            this.SizeSelect.TabIndex = 2;
+            this.SizeSelect.Value = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Glyph Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "px";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 67);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SizeSelect);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textbox);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "Ray Notation Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.SizeSelect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown SizeSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RayNotationGenerator/Main.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RayNotationGenerator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public void Encode()
+        {
+            byte[] bytes = System.Text.Encoding.Default.GetBytes(textbox.Text);
+            List<Image> images = new List<Image>();
+
+            int imageSize = (int) SizeSelect.Value;
+
+            int iter = 1;
+
+            foreach (byte b in bytes)
+            {
+                // Create a new image
+                Image img = new Bitmap(imageSize, imageSize);
+
+                Graphics drawing = Graphics.FromImage(img);
+
+                // Paint the background
+                drawing.Clear(Color.FromArgb(0, 33, 33, 33));
+
+                // QR-like rectangles
+                Point centre     = new Point(img.Width / 2, img.Height / 2);
+
+                Point pos0Finish    = new Point(img.Width/2     , 0             );
+                Point pos1Finish    = new Point(img.Width       , 0             );
+                Point pos2Finish    = new Point(img.Width       , img.Height/2  );
+                Point pos3Finish    = new Point(img.Width       , img.Height    );
+                Point pos4Finish    = new Point(img.Width/2     , img.Height    );
+                Point pos5Finish    = new Point(0               , img.Height    );
+                Point pos6Finish    = new Point(0               , img.Height/2  );
+                Point pos7Finish    = new Point(0               , 0             );
+                
+                // Determine what colour everything is
+                Pen pos0C = (b & (1 << 0)) != 0 ? new Pen(Color.FromArgb(255, 0, 0, 0), imageSize/10) : new Pen(Color.FromArgb(0, 0, 0, 0), imageSize / 10);
+                Pen pos1C = (b & (1 << 1)) != 0 ? new Pen(Color.FromArgb(255, 0, 0, 0), imageSize / 10) : new Pen(Color.FromArgb(0, 0, 0, 0), imageSize / 10);
+                Pen pos2C = (b & (1 << 2)) != 0 ? new Pen(Color.FromArgb(255, 0, 0, 0), imageSize / 10) : new Pen(Color.FromArgb(0, 0, 0, 0), imageSize / 10);
+                Pen pos3C = (b & (1 << 3)) != 0 ? new Pen(Color.FromArgb(255, 0, 0, 0), imageSize / 10) : new Pen(Color.FromArgb(0, 0, 0, 0), imageSize / 10);
+                Pen pos4C = (b & (1 << 4)) != 0 ? new Pen(Color.FromArgb(255, 0, 0, 0), imageSize / 10) : new Pen(Color.FromArgb(0, 0, 0, 0), imageSize / 10);
+                Pen pos5C = (b & (1 << 5)) != 0 ? new Pen(Color.FromArgb(255, 0, 0, 0), imageSize / 10) : new Pen(Color.FromArgb(0, 0, 0, 0), imageSize / 10);
+                Pen pos6C = (b & (1 << 6)) != 0 ? new Pen(Color.FromArgb(255, 0, 0, 0), imageSize / 10) : new Pen(Color.FromArgb(0, 0, 0, 0), imageSize / 10);
+                Pen pos7C = (b & (1 << 7)) != 0 ? new Pen(Color.FromArgb(255, 0, 0, 0), imageSize / 10) : new Pen(Color.FromArgb(0, 0, 0, 0), imageSize / 10);
+
+                // Draw rectangles and text
+                drawing.DrawLine(pos0C, centre, pos0Finish);
+                drawing.DrawLine(pos1C, centre, pos1Finish);
+                drawing.DrawLine(pos2C, centre, pos2Finish);
+                drawing.DrawLine(pos3C, centre, pos3Finish);
+                drawing.DrawLine(pos4C, centre, pos4Finish);
+                drawing.DrawLine(pos5C, centre, pos5Finish);
+                drawing.DrawLine(pos6C, centre, pos6Finish);
+                drawing.DrawLine(pos7C, centre, pos7Finish);
+
+                // Finalise drawing
+                drawing.Save();
+
+                // Dispose of unused objects
+                pos0C.Dispose();
+                pos1C.Dispose();
+                pos2C.Dispose();
+                pos3C.Dispose();
+                pos4C.Dispose();
+                pos5C.Dispose();
+                pos6C.Dispose();
+                pos7C.Dispose();
+
+                drawing.Dispose();
+
+                images.Add(img);
+
+                iter++;
+            }
+
+
+
+            Image combined = new Bitmap((images.Count * imageSize) + (images.Count * (imageSize / 8)), imageSize);
+            Graphics combinedDrawing = Graphics.FromImage(combined);
+
+            int x = 0;
+            foreach (Image img in images)
+            {
+                combinedDrawing.DrawImage(img, x, 0);
+                x += imageSize + imageSize / 8;
+            }
+            combinedDrawing.Save();
+            combinedDrawing.Dispose();
+
+            FileDialog fd = new SaveFileDialog();
+            fd.Filter = "PNG|*.png";
+            fd.ShowDialog();
+
+           combined.Save(fd.FileName, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void Encode(object sender, EventArgs e)
+        {
+            if (textbox.Text == "")
+            {
+                MessageBox.Show("Enter something to encode!");
+            } else
+            {
+                Encode();
+            }
+        }
+    }
+}
